8dcd7c68ecefb8c9519a075120b4f534384a8a0f
안선영 <74126735+seonyoungan@users.noreply.github.com>
2022-03-05 00:03:45 +0900

    Update cycle_fibonacci01.c

--- C/cycle_fibonacci01.c
+++ C/cycle_fibonacci01.c
@@ -6,11 +6,23 @@ int main(){
     int ret =0;
     ret = recursive(5);
     printf("반환값: %d", ret);
+    
+    return 0;
 }
 
 
 int recursive(int n){
     printf("현재 n값: %d", n);
     if(n<1) return 2;
-    else return (2*(n-1)+1);
+    else return (2*recursive(n-1)+1);
 }
+
+/*결과값
+현재 n값: 5
+현재 n값: 4
+현재 n값: 3
+현재 n값: 2
+현재 n값: 1
+현재 n값: 0
+반환값: 95
+*/
