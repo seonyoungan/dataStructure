d4736bfe6b1768f227b7901e0b85b752e18a32aa
안선영 <74126735+seonyoungan@users.noreply.github.com>
2022-04-08 02:48:15 +0900

    update stusdy llinkedList

--- C/linkedList_simple.c
+++ C/linkedList_simple.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int element;
-typedef struct ListNode{
-    element data; //노드
-    struct ListNode *link; // 링크_포인터변수
+typedef int element; //정수형 데이터
+typedef struct listNode{
+    element data; //정수타입의 노드
+    struct listNode *link; //포인트변수의 링크
 } listNode;
 
 void erorr(char *message){
@@ -13,43 +13,53 @@ void erorr(char *message){
 }
 
 listNode* insert_first(listNode *head, int value){
-    listNode *p = (listNode *)mallock(sizeof(listNode)); // 리스트노드 크기만큼의 메모리할당
-    p->data = value; // value를 data변수에
-    p->link = head; // head(포인터값) 변수link (포인터변수)에 복사
-    head = p; // p값을 head에 대입 (= head 포인터 변경)
-    return head; //변경된 head 반환
+    listNode *p = (listNode *)malloc(sizeof(listNode));  // p는 동적메모리 listnodde를 가리킴
+    p->data = value; // value값을 p의 데이터필드에 복사
+    p->link = head; // link는 head를 가리킴(참조)
+    head = p; // head가 p를 가리킴
+    return head; //head를 반환
+    
 } //데이터값 입력 후 head포인터 변경
+
 listNode* insert(listNode *head, listNode *pre, element value){
-    listNode *p = (listNode *)malloc(sizeof(listNode));
-    p->data = value;
-    p->link = pre->link;
-    pre->link = p; //?
-    return head;
+    listNode *p = (listNode *)malloc(sizeof(listNode)); // p는 동적메모리 listnodde를 가리킴
+    p->data = value; // value값을 p의 데이터필드에 복사
+    p->link = pre->link; // p의 링크(p의 다음데이터)가 pre의 링크(pre의 다음데이터)를 가리킴
+    pre->link = p; //pre링크(pre의 다음데이터)가 p를 가리킴
+    return head; //head반환
 }
 
-int delete(listNode *head, listNode *pre){
+listNode* del_first(listNode *head){
+    listNode *removed; //removed라는 포인터변수생성
+    if(head == NULL) //head가 null이면
+        return NULL; //null을 리턴
+    removed = head; // removed가 head를 가리킴
+    head = removed->link; //head는 remove의 링크(remove의 다음데이터를 가리킴
+    free(removed); //removed 동적메모리해제(=head를 가리킸지!)
+    return head; //head반환
+}
+listNode* del(listNode *head, listNode *pre){
     listNode *removed; //removed라는 포인터변수 생성
-    removed = pre->link;
-    pre->link = removed->link;
-    free(removed);
-    return head;
+    removed = pre->link; //removed는 pre의 링크(=pre 다음data)를 가리킴
+    pre->link = removed->link; // pre의 링크는 removed의 링크(=removed의 다음data)를 가리킴
+    free(removed); //동적메모리해제 (=삭제)
+    return head; //head반환
 }
 
 void print_list(listNode *head){
-    for(listNode *p = head; p!=NULLl; p=p->link)
-        printf("%d->", p->data);
-    printf("NULL\n");
+    for(listNode *p = head; p!=NULL; p=p->link) // p가 null이 아닐때까지 p는 p의 링크(=다음데이터)로 이동하며(?) 반복
+        printf("%d->", p->data); //출력
+    printf("NULL\n"); //null출력
 }
-
-
 
 int main() {
     listNode *head = NULL;
     for(int i = 0; i < 5; i++){
         head = insert_first(head, i);
         print_list(head);
+    }
     for(int i = 0; i < 5; i++){
-        head = delete(head);
+        head = del_first(head);
         print_list(head);
     }
     return 0;
