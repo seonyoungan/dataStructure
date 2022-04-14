85afc94af4589c171bb033eeb253622ccc2f2a26
안선영 <74126735+seonyoungan@users.noreply.github.com>
2022-04-15 05:32:02 +0900

    update study pointer

--- C/basic_pointer.c
+++ C/basic_pointer.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #define SIZE 6
-void get_integers(int list[]){
+void get_integers(int list[]){ // 배열을 매개변수로 받음
     printf("6개의 정수 입력하세요.: ");
     for(int i=0;i<SIZE;i++){
-        scanf("%d", &list[i]); // 입력한 값이 list배열의 0~5번째 원소에 순차적으로 들어가겠지..
+        scanf("%d", &list[i]); // 입력한 값이 list배열의 0~5번째 원소에 순차적으로 들어감..
     }
 }
 int cal_sum(int list[]){
     int sum=0; //sum 선언, 0으로 값 초기화
-    for(int i=0;i<SIZE;i++){ //넘겨진값+1
-        sum = sum + *(list + i); //
+    for(int i=0;i<SIZE;i++){ //i<6일 때까지 반복(i값은 1씩 증가됨)
+        sum += sum *(list + i); // sum = sum + list+i주소가 가리키는 곳의 값
     }
-    return sum;
+    return sum; //누적된 합계
 }
 
 int main() {
-    int list[SIZE]; // SIZE크기만큼의 배열 선언
-    get_integers(list); //0~5번째 원소값 불려와진다
+    int list[SIZE]; //1. list[6] 배열 생성, list = list[0]의 주소
+    get_integers(list); //2. list 주소를 get_integers의 인자로 넘긴다.
     printf("합: %d\n", cal_sum(list));
 }
